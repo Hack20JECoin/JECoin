2ac4e73e2c6a3c0b3ebbd3cd53ee3c47815a5f4c
Howard Wallis <howard.wallis@just-eat.com>
2017-10-04 16:58:05 +0100

    tests in progress

--- test/TestJECoin.sol
+++ test/TestJECoin.sol
@@ -6,20 +6,26 @@ import "../contracts/JECoin.sol";
 
 contract TestJECoin {
 
-  function testInitialBalanceUsingDeployedContract() {
-    JECoin coin = JECoin(DeployedAddresses.JECoin());
+  JECoin TestJECoin;
+  address one;
+  address two;
 
-    uint expected = 1000000000;
-
-    Assert.equal(coin.balanceOf(tx.origin), expected, "Owner should have 1000000000 JECoin initially");
+  function beforeAll() {
+    TestJECoin = JECoin(DeployedAddresses.JECoin());
+    one = 0x1111111111111111111111111111111111111111;
+    two = 0x2222222222222222222222222222222222222222;
+    TestJECoin.transfer(one, 1);
+    TestJECoin.transfer(two, 2);
   }
 
-  function testInitialBalanceWithNewJECoin() {
-    JECoin coin = new JECoin();
-
+  function testInitialBalanceUsingDeployedContract() {
     uint expected = 1000000000;
 
-    Assert.equal(coin.balanceOf(tx.origin), expected, "Owner should have 1000000000 JECoin initially");
+    Assert.equal(TestJECoin.balanceOf(msg.sender), expected, "Owner should have 1000000000 JECoin initially");
+  }
+
+  function testBalances() {
+    Assert.equal(TestJECoin.balanceOf(one), 1, "Expected one coin");
   }
 
 }
