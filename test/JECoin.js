555f7844d5155df8d83e13e6770a248e8d6948d2
Howard Wallis <howard.wallis@just-eat.com>
2017-10-05 11:47:15 +0100

    test username crud methods

--- test/JECoin.js
+++ test/JECoin.js
@@ -3,7 +3,11 @@ var JECoin = artifacts.require("./JECoin.sol");
 contract('JECoin', function(accounts) {
   console.log('available accounts: ', accounts);
   const owner = accounts[0];
-  const one = accounts[1];
+  const ownerName = "owner";
+  const accountOne = accounts[1];
+  const accountOneName = "one";
+  const accountTwo = accounts[2];
+  const accountTwoName = "two";
 
   it("should put 1000000000 JECoin in the first account", function() {
     return JECoin.deployed().then(function(instance) {
@@ -14,35 +18,78 @@ contract('JECoin', function(accounts) {
   });
 
   it("should send coin correctly", function() {
-    var coin;
+    let coin;
 
     // Get initial balances of first and second account.
-    var owner_starting_balance;
-    var one_starting_balance;
-    var owner_ending_balance;
-    var one_ending_balance;
+    let owner_starting_balance;
+    let accountOne_starting_balance;
+    let owner_ending_balance;
+    let accountOne_ending_balance;
 
-    var amount = 10;
+    let amount = 10;
 
     return JECoin.deployed().then(function(instance) {
       coin = instance;
       return coin.balanceOf.call(owner);
     }).then(function(balance) {
       owner_starting_balance = balance.toNumber();
-      return coin.balanceOf.call(one);
+      return coin.balanceOf.call(accountOne);
     }).then(function(balance) {
-      one_starting_balance = balance.toNumber();
-      return coin.transfer(one, amount, {from: owner});
+      accountOne_starting_balance = balance.toNumber();
+      return coin.transfer(accountOne, amount, {from: owner});
     }).then(function() {
       return coin.balanceOf.call(owner);
     }).then(function(balance) {
       owner_ending_balance = balance.toNumber();
-      return coin.balanceOf.call(one);
+      return coin.balanceOf.call(accountOne);
     }).then(function(balance) {
-      one_ending_balance = balance.toNumber();
+      accountOne_ending_balance = balance.toNumber();
 
       assert.equal(owner_ending_balance, owner_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-      assert.equal(one_ending_balance, one_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+      assert.equal(accountOne_ending_balance, accountOne_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+    });
+  });
+
+  it("can register and read usernames", function() {
+    let coin;
+
+    return JECoin.deployed().then(function(instance) {
+      coin = instance;
+      return coin.registerUsername.call(ownerName, owner);
+    }).then(function() {
+      return coin.addressOf.call(ownerName)
+    }).then(function(address) {
+      assert.equal(address, owner, "Owner name wasn't registered or wasn't read.");
+    });
+  });
+
+  it("can change a registered username", function() {
+    let coin;
+
+    return JECoin.deployed().then(function(instance) {
+      coin = instance;
+      return coin.registerUsername.call(accountOneName, accountOne);
+    }).then(function() {
+      return coin.changeUsername.call(accountOneName, ownerName)
+    }).then(function(success) {
+      return coin.addressOf.call(ownerName);
+    }).then(function(address) {
+      assert.equal(address, owner, "Owner didn't take over account one");
+    });
+  });
+
+  it("can delete a registered username", function() {
+    let coin;
+
+    return JECoin.deployed().then(function(instance) {
+      coin = instance;
+      return coin.registerUsername.call(accountTwoName, accountTwo);
+    }).then(function() {
+      return coin.deleteUsername.call(accountTwoName)
+    }).then(function() {
+      return coin.addressOf.call(accountTwoName);
+    }).then(function(address) {
+      assert.equal(address, 0x0, "Account one name wasn't deleted");
     });
   });
 });
