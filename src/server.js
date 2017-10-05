0111103f60469b92d1d56f918e0524a3e6769aa1
Berk Mollamustafaoglu <berkmolla@gmail.com>
2017-10-05 13:55:19 +0100

    Remove commented out code.

--- src/server.js
+++ src/server.js
@@ -27,29 +27,6 @@ function processEvent(repo, ref, data) {
 
 app.use('/webhook', require('express-github-hook')(options));
 
-// Set up server options
-// const serverOptions = {host: '127.0.0.1', path: '/webhook', port: 3000}
-// const githubhookListener = require('githubhook')(serverOptions)
-
-// Start server
-// githubhookListener.listen()
-
-// Listen for pull request events on the repo
-// githubhookListener.on('pull_request:JECoin', (ref, data) => {
-//
-//     const action = data.action
-//
-//     // If PR is merged
-//     if (action === 'closed' && data.pull_request.merged === true) {
-//
-//     }
-//
-//     console.log(data);
-//
-// });
-
-
-
 app.get('/cheer', () => {
     console.log('cheer');
 })
