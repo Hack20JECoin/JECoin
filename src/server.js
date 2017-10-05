8e7989b03c1d74e501b29fb450b0f1b1651de4e0
Berk Mollamustafaoglu <berkmolla@gmail.com>
2017-10-05 13:53:46 +0100

    Change server to use express, and switch to using express-github-hook package instead of standard githubhook package.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 const http = require('http');
+const express = require('express')
+const app = express()
 
-// Set up server options
-const serverOptions = {host: '127.0.0.1', path: '/webhook', port: 3000}
-const githubhookListener = require('githubhook')(serverOptions)
+const port = 3000;
 
-// Start server
-githubhookListener.listen()
+const options = {
+    treatRequest: function(event, repo, ref, data) {
+        switch(event) {
+            case 'pull_request':
+                processEvent(repo, ref, data);
+                break;
+        }
+    }
+}
 
-// Listen for pull request events on the repo
-githubhookListener.on('pull_request:JECoin', (ref, data) => {
-
+function processEvent(repo, ref, data) {
     const action = data.action
 
     // If PR is merged
@@ -18,5 +23,37 @@ githubhookListener.on('pull_request:JECoin', (ref, data) => {
     }
 
     console.log(data);
+}
 
-});
+app.use('/webhook', require('express-github-hook')(options));
+
+// Set up server options
+// const serverOptions = {host: '127.0.0.1', path: '/webhook', port: 3000}
+// const githubhookListener = require('githubhook')(serverOptions)
+
+// Start server
+// githubhookListener.listen()
+
+// Listen for pull request events on the repo
+// githubhookListener.on('pull_request:JECoin', (ref, data) => {
+//
+//     const action = data.action
+//
+//     // If PR is merged
+//     if (action === 'closed' && data.pull_request.merged === true) {
+//
+//     }
+//
+//     console.log(data);
+//
+// });
+
+
+
+app.get('/cheer', () => {
+    console.log('cheer');
+})
+
+app.listen(port, () => {
+    console.log('App is now listening on port 3000');
+})
