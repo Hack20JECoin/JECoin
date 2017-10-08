cedf977060d3b01d1b842f449fa2c1c8b5093a04
Berk Mollamustafaoglu <berkmolla@gmail.com>
2017-10-08 22:33:45 +0100

    Create bounty when PR is created (how do I test this properly? D:)

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var web3 = new Web3();
 var provider = web3.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
 // web3.eth.defaultAccount=web3.eth.accounts[0]
 
-const coordinatorAddress = '0x742e82e5cc14ed9813513f1357cbe047acca4f71'
+const coordinatorAddress = '0xff87c1fb8008cff772ed0ecc561f3c56e1b8b086'
 
 // Set up contracts
 const TruffleContract = require('truffle-contract');
@@ -17,6 +17,8 @@ const JECoin = TruffleContract(JECoinContract);
 
 JECoin.setProvider(web3.currentProvider)
 Coordinator.setProvider(web3.currentProvider)
+
+web3.eth.defaultAccount = web3.eth.coinbase;
 
 if (typeof JECoin.currentProvider.sendAsync !== "function") {
     JECoin.currentProvider.sendAsync = function() {
@@ -52,8 +54,13 @@ function processEvent(repo, ref, data) {
     const repoName = data.repository.name
     const repoOwner = data.repository.owner.login
     const pullRequestNumber = data.pull_request.number
-    const pullRequestAuthor = data.pull_request.owner.login
+    const pullRequestAuthor = data.pull_request.user.login
     const isMerged = data.pull_request.merged
+
+    console.log(repoName)
+    console.log(repoOwner)
+    console.log(pullRequestNumber)
+    console.log(pullRequestAuthor)
 
     // If PR is merged
     if (action === 'closed' && isMerged === true) {
@@ -65,13 +72,16 @@ function processEvent(repo, ref, data) {
     if (action === 'opened' || action === 'reopened') {
 
         Coordinator.at(coordinatorAddress).then(coordinator => {
-            coordinator.createBounty(repoOwner, repoName, pullRequestNumber, pullRequestAuthor)
+            coordinator.createBounty.call(repoOwner, repoName, pullRequestNumber, pullRequestAuthor)
             .then(() => console.log('Successfully created bounty'))
             .catch(err => console.log(`Error: ${err}`));
+        })
+        .catch(err => {
+            console.log(err);
         });
     }
 
-    console.log(data);
+    // console.log(data);
 }
 
 app.use('/webhook', require('express-github-hook')(options));
